0667ad8fe3fbcd7d8879c1c263f42084ca567dcd
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-08-10 01:52:06 +0500

    welcome page is in progress

--- react/src/Components/Welcome.jsx
+++ react/src/Components/Welcome.jsx
@@ -1,3 +1,5 @@
+// import React from 'react';
+// import {link} from "react-router-dom"
 import logo from '../assets/Eduverse-logo.png'
 function Welcome(){
     return(<>
@@ -8,11 +10,28 @@ function Welcome(){
             <button id="signup">Sign up</button>
         </div>
     </div>
+    <div className="box">
     <div className="left">
+        <h1 id='connect'>Connect.</h1>
+        <h1 id='learn'>Learn.</h1>
+        <h1 id='grow'>Grow.</h1>
+        <p className="description">Eduverse is an educational <br />
+        social network for students, <br />
+        teachers and institutes to <br />
+        connect, share and learn together.</p>
+
+        <button id='start'>Get Started</button>
+        <button id='explore'>Explore</button>
         
+
     </div>
     <div className="right">
 
+    </div>
+
+    <footer id='footer'>
+        
+    </footer>
     </div>
     </>)
 }
